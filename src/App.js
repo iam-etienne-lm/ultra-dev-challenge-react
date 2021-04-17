7d23557f3e0f55f7b9cfd9e66b1e84e892aec3bc
etienne.leroy <etienne.leroy@epitech.eu>
2021-04-17 18:18:45 +0200

    ADD - arrow function ok but State ko

--- src/App.js
+++ src/App.js
@@ -1,46 +1,82 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
-function Title() {
-  return <p className="title">Cucara Chat app [followed antennas here]</p>;
+const MOCK_DATA = [
+  {
+    cucId: "xXdarkcrawlerXx",
+    text: "found some stuff, left a path",
+    antennas: "food"
+  },
+  {
+    Id: "Elon Chplutchk",
+    text: "sell doge",
+    antennas: "crypto"
+  }
+];
+
+const Title = () => {
+  return <p className="title">Cucara Chat app "props.antennas.id"</p>;
 }
 
-function Welcome(props) {
+const Welcome = (props) => {
   return <h3>Hello, {props.name}</h3>;
 }
 
-function MessageBoard() {
+const MessageBoard = () => {
   return (
-    <ul className="messageboard">
-      <li>message1 message.id.1 antenna.id.1 cucaId.1</li>
-      <li>message2 message.id.2 antenna.id.2 cucaId.2</li>
-      <li>message2 message.id.3 antenna.id.3 cucaId.3</li>
-      <div classname="justlanded">
-        <Welcome name="2-fast-2-sprotch" />
-        <Welcome name="xXdarkcrawlerXx" />
-        <Welcome name="Elon Chplutchk" />
-      </div>
-    </ul>
-  );
-}
-
-function RadioBox() {
-  return (
-    <form
-    className="radioboxform">
-    <input
-        placeholder="Type message and froutch antennas |_>"
-        type="text" />
-    </form>
-  )
-}
-
-export default function App() {
-  return (
-    <div className="app">
-      <Title />
-      <MessageBoard />
-      <RadioBox/>
+    <div>
+      <ul className="messageboard1">
+        <li>message1 message.id.1 antenna.id.1 cucId.1</li>
+        <li>message2 message.id.2 antenna.id.2 cucId.2</li>
+        <li>message2 message.id.3 antenna.id.3 cucId.3</li>
+        <div classname="justlanded">
+          <Welcome name="2-fast-2-sprotch" />
+          <Welcome name="xXdarkcrawlerXx" />
+          <Welcome name="Elon Chplutchk" />
+        </div>
+      </ul>
+      <ul className="messageboard">                 
+              {/*{this.props.messages.map(message => {
+                return (
+                <li key={message.id}>
+                  <div>
+                    {message.cucId}
+                  </div>
+                  <div>
+                    {message.text}
+                  </div>
+                </li>
+                
+              )
+            })}
+            */}
+      </ul>
     </div>
   );
 }
+
+const RadioBox = () => {
+  return (
+    <form className="radioboxform">
+      <input placeholder="Type message and froutch antennas |_>" type="text" />
+    </form>
+  );
+}
+
+const App = () => {
+  const [messages, setMessages] = React.useState(MOCK_DATA)
+
+  return (
+    <div className="app">
+      <Title />
+      <MessageBoard setMessages ={() =>
+        this.setState=((state) => {
+          return {messages: state.messages};
+        })
+      }/>
+      <RadioBox />
+    </div>
+  );
+}
+
+export default App;
